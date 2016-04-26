9d2d3b00fd74c7d8548b43db47be0b024278a6d3
zhangsiqiang07 <1328254616@qq.com>
2016-04-26 09:17:54 +0800

    一键支付页面

---
+++ Parking/首页/view/TCSingleClickPayCell.swift
@@ -0,0 +1,24 @@
+//
+//  TCSingleClickPayCell.swift
+//  Parking
+//
+//  Created by xiaocool on 16/4/25.
+//  Copyright © 2016年 北京校酷网络科技有限公司. All rights reserved.
+//
+
+import UIKit
+
+class TCSingleClickPayCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
